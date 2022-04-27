51f7e958e467283a0f41de1c2f2b018e8dc414a8
ayrton <ayrton.parkinson1@gmail.com>
2022-04-27 18:04:36 -0400

    Initial commit

---
+++ app/controllers/stims_controller.rb
@@ -0,0 +1,3 @@
+class StimsController < ApplicationController
+  def index; end
+end
---
+++ test/controllers/stims_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class StimsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
